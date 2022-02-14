c17bbf37253c5d8adcd24363792926e84ce15461
Ian Tang <ian.tang@protonmail.com>
2022-02-14 12:49:32 -0500

    improve formatting using autopep, remove extraneous files

--- gui_proto.py
+++ gui_proto.py
@@ -3,6 +3,7 @@ from math import sin, cos, pi, sqrt
 from numpy import arange
 import wheelbuild
 
+
 class Draw(Frame):
     def __init__(self):
         super().__init__()
@@ -19,8 +20,8 @@ class Draw(Frame):
         cross = 2
 
         # define the angle in degrees of each hole in the hub/rim
-        spokes_drive = arange(-180.0/spoke_count, -360, -720.0/spoke_count)
-        spokes_nondrive = arange(180.0/spoke_count, 360, 720.0/spoke_count)
+        spokes_drive = arange(-180.0 / spoke_count, -360, -720.0 / spoke_count)
+        spokes_nondrive = arange(180.0 / spoke_count, 360, 720.0 / spoke_count)
 
         # convert angle to 2 dimensional coordinates based on rim and hub flange diameter
         hub_coords_drive = []
@@ -28,19 +29,23 @@ class Draw(Frame):
         rim_coords_drive = []
         rim_coords_nondrive = []
         for i in range(len(spokes_drive)):
-            hub_coords_drive.append([flange_diameter/2 * sin(spokes_drive[i] * pi/180), flange_diameter/2 * cos(spokes_drive[i] * pi/180)])
-            rim_coords_drive.append([effective_rim_diameter/2 * sin(spokes_drive[i] * pi/180), effective_rim_diameter/2 * cos(spokes_drive[i] * pi/180)])
+            hub_coords_drive.append([flange_diameter / 2 * sin(spokes_drive[i]
+                                    * pi/180), flange_diameter / 2 * cos(spokes_drive[i] * pi / 180)])
+            rim_coords_drive.append([effective_rim_diameter / 2 * sin(
+                spokes_drive[i] * pi / 180), effective_rim_diameter / 2 * cos(spokes_drive[i] * pi / 180)])
         for i in range(len(spokes_nondrive)):
-            hub_coords_nondrive.append([flange_diameter/2 * sin(spokes_nondrive[i] * pi/180), flange_diameter/2 * cos(spokes_nondrive[i] * pi/180)])
-            rim_coords_nondrive.append([effective_rim_diameter/2 * sin(spokes_nondrive[i] * pi/180), effective_rim_diameter/2 * cos(spokes_nondrive[i] * pi/180)])
+            hub_coords_nondrive.append([flange_diameter / 2 * sin(
+                spokes_nondrive[i] * pi / 180), flange_diameter / 2 * cos(spokes_nondrive[i] * pi / 180)])
+            rim_coords_nondrive.append([effective_rim_diameter / 2 * sin(
+                spokes_nondrive[i] * pi / 180), effective_rim_diameter / 2 * cos(spokes_nondrive[i] * pi / 180)])
 
         # canvas dimensions
         global canvas_x
         global canvas_y
         canvas_x = 900
         canvas_y = 750
-        center = [canvas_x/2, canvas_y/2]
-        oval_size = 2 # radius of hub spoke holes in the drawing
+        center = [canvas_x / 2, canvas_y / 2]
+        oval_size = 2  # radius of hub spoke holes in the drawing
 
         # draw the spoke holes in the hub
         # driveside spoke holes are in black and nondriveside are in red
@@ -48,37 +53,39 @@ class Draw(Frame):
         for i in range(len(spokes_drive)):
             x = hub_coords_drive[i][0]
             y = hub_coords_drive[i][1]
-            canvas.create_oval(center[0]+x-oval_size, center[1]-y-oval_size, center[0]+x+oval_size, center[1]-y+oval_size, fill="black")
+            canvas.create_oval(center[0] + x - oval_size, center[1] - y - oval_size,
+                               center[0] + x + oval_size, center[1] - y + oval_size, fill="black")
         for i in range(len(spokes_nondrive)):
             x = hub_coords_nondrive[i][0]
             y = hub_coords_nondrive[i][1]
-            canvas.create_oval(center[0]+x-oval_size, center[1]-y-oval_size, center[0]+x+oval_size, center[1]-y+oval_size, fill="red")
+            canvas.create_oval(center[0] + x - oval_size, center[1] - y - oval_size,
+                               center[0] + x + oval_size, center[1] - y + oval_size, fill="red")
 
         # draw the rim and hub circles
-        canvas.create_oval(center[0]-effective_rim_diameter/2, center[1]-effective_rim_diameter/2,\
-            center[0]+effective_rim_diameter/2, center[1]+effective_rim_diameter/2, width=3)
-        canvas.create_oval(center[0]-flange_diameter/2-5, center[1]-flange_diameter/2-5,\
-            center[0]+flange_diameter/2+5, center[1]+flange_diameter/2+5, width=2)
-        
+        canvas.create_oval(center[0] - effective_rim_diameter / 2, center[1] - effective_rim_diameter / 2,
+                           center[0] + effective_rim_diameter / 2, center[1] + effective_rim_diameter / 2, width=3)
+        canvas.create_oval(center[0] - flange_diameter / 2 - 5, center[1] - flange_diameter / 2 - 5,
+                           center[0] + flange_diameter / 2 + 5, center[1] + flange_diameter / 2 + 5, width=2)
+
         # draw each spoke
         for i in range(len(spokes_drive)):
-            if i%2 == 0:
-                canvas.create_line(center[0]+hub_coords_drive[(i+cross)%len(spokes_drive)][0], center[1]-hub_coords_drive[(i+cross)%len(spokes_drive)][1],\
-                    center[0]+rim_coords_drive[i][0],\
-                    center[1]-rim_coords_drive[i][1], width=2)
+            if i % 2 == 0:
+                canvas.create_line(center[0] + hub_coords_drive[(i + cross) % len(spokes_drive)][0], center[1]-hub_coords_drive[(i + cross) % len(spokes_drive)][1],
+                                   center[0] + rim_coords_drive[i][0],
+                                   center[1] - rim_coords_drive[i][1], width=2)
             else:
-                canvas.create_line(center[0]+hub_coords_drive[(i-cross)%len(spokes_drive)][0], center[1]-hub_coords_drive[(i-cross)%len(spokes_drive)][1],\
-                    center[0]+rim_coords_drive[i][0],\
-                    center[1]-rim_coords_drive[i][1], width=2)
+                canvas.create_line(center[0] + hub_coords_drive[(i - cross) % len(spokes_drive)][0], center[1]-hub_coords_drive[(i - cross) % len(spokes_drive)][1],
+                                   center[0] + rim_coords_drive[i][0],
+                                   center[1] - rim_coords_drive[i][1], width=2)
         for i in range(len(spokes_nondrive)):
-            if i%2 == 0:
-                canvas.create_line(center[0]+hub_coords_nondrive[(i+cross)%len(spokes_nondrive)][0], center[1]-hub_coords_nondrive[(i+cross)%len(spokes_nondrive)][1],\
-                    center[0]+rim_coords_nondrive[i][0],\
-                    center[1]-rim_coords_nondrive[i][1], width=2, fill="#ed5353")
+            if i % 2 == 0:
+                canvas.create_line(center[0] + hub_coords_nondrive[(i + cross) % len(spokes_nondrive)][0], center[1] - hub_coords_nondrive[(i + cross) % len(spokes_nondrive)][1],
+                                   center[0] + rim_coords_nondrive[i][0],
+                                   center[1] - rim_coords_nondrive[i][1], width=2, fill="#ed5353")
             else:
-                canvas.create_line(center[0]+hub_coords_nondrive[(i-cross)%len(spokes_nondrive)][0], center[1]-hub_coords_nondrive[(i-cross)%len(spokes_nondrive)][1],\
-                    center[0]+rim_coords_nondrive[i][0],\
-                    center[1]-rim_coords_nondrive[i][1], width=2, fill="#ed5353")
+                canvas.create_line(center[0] + hub_coords_nondrive[(i - cross) % len(spokes_nondrive)][0], center[1] - hub_coords_nondrive[(i - cross) % len(spokes_nondrive)][1],
+                                   center[0] + rim_coords_nondrive[i][0],
+                                   center[1] - rim_coords_nondrive[i][1], width=2, fill="#ed5353")
 
         canvas.pack(fill=BOTH, expand=1)
 
@@ -88,6 +95,7 @@ def main():
     draw = Draw()
     root.geometry(str(canvas_x) + "x" + str(canvas_y))
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
--- wheelbuild.py
+++ wheelbuild.py
@@ -2,24 +2,35 @@ import math
 import argparse
 import sys
 
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("flange_diameter", help="flange diameter in mm", type=float)
-    parser.add_argument("center_flange_distance", help="distance from flange to hub centerline in mm", type=float)
-    parser.add_argument("effective_rim_diameter", help="diameter of spoke bed in rim, in mm", type=float)
-    parser.add_argument("--spokes", help="number of spokes", type=int, default=32)
-    parser.add_argument("--cross", help="lacing pattern, omit for radial lacing", type=int, default=0)
+    parser.add_argument("flange_diameter",
+                        help="flange diameter in mm", type=float)
+    parser.add_argument("center_flange_distance",
+                        help="distance from flange to hub centerline in mm", type=float)
+    parser.add_argument("effective_rim_diameter",
+                        help="diameter of spoke bed in rim, in mm", type=float)
+    parser.add_argument("--spokes", help="number of spokes",
+                        type=int, default=32)
+    parser.add_argument(
+        "--cross", help="lacing pattern, omit for radial lacing", type=int, default=0)
     args = parser.parse_args()
-    print(calc_spoke_length(args.flange_diameter, args.center_flange_distance, args.effective_rim_diameter, args.spokes, args.cross))
+    print(calc_spoke_length(args.flange_diameter, args.center_flange_distance,
+          args.effective_rim_diameter, args.spokes, args.cross))
+
 
 def main_interactive():
     flange_diameter = float(input("Enter flange diameter: "))
     center_flange_distance = float(input("Enter center-flange distance: "))
     effective_rim_diameter = float(input("Enter ERD: "))
     spoke_count = float(input("Enter number of spokes: "))
-    cross = int(input("Enter lacing pattern, between 1 and 4. Enter 0 for radial lacing: "))
-    spoke_length = calc_spoke_length(flange_diameter, center_flange_distance, effective_rim_diameter, spoke_count, cross)
+    cross = int(
+        input("Enter lacing pattern, between 1 and 4. Enter 0 for radial lacing: "))
+    spoke_length = calc_spoke_length(
+        flange_diameter, center_flange_distance, effective_rim_diameter, spoke_count, cross)
     print("Spoke length is {:.1f}".format(spoke_length))
+
 
 def calc_spoke_length(fd, cfd, erd, spokes, cross):
     if fd <= 0 or cfd < 0 or erd <= 0 or spokes <= 0 or cross < 0:
@@ -35,6 +46,7 @@ def calc_spoke_length(fd, cfd, erd, spokes, cross):
     else:
         angle = 720.0/spokes * cross
         return math.sqrt(math.pow(erd/2.0, 2) + math.pow(fd/2.0, 2) - ((erd * fd)/2.0) * math.cos(angle * math.pi/180) + math.pow(cfd, 2))
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
